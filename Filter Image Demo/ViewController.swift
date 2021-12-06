79c966097f9d7fcfaff439da5f95f710ee7621a2
maged <maged12345>
2021-12-06 09:13:38 -0800

    initial commit for finishing design

--- Filter Image Demo/ViewController.swift
+++ Filter Image Demo/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var intenditySlider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var currentImage:UIImage!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Filter Image"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+    }
+    @objc func importPicture ( ) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil )
     }
 
-
+    @IBAction func changeFilterButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+    }
+    
 }
 
