47c60204c6144f0956c8b4c96c5007866c418438
maged <maged12345>
2021-12-06 11:01:31 -0800

    adding save button and execute the function

--- Filter Image Demo/ViewController.swift
+++ Filter Image Demo/ViewController.swift
@@ -72,9 +72,8 @@ class ViewController: UIViewController {
         
             applyProcessing()
        }
-    @IBAction func saveButton(_ sender: UIButton) {
-        
-    }
+   
+    
     @IBAction func changeFilterButton(_ sender: UIButton) {
         let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
@@ -91,6 +90,24 @@ class ViewController: UIViewController {
         }
         present(ac, animated: true)
     }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    @IBAction func saveButton(_ sender: UIButton) {
+            guard let image = imageView.image else { return }
+
+               UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+           }
 }
 
 extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate {
