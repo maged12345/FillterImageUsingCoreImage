55cfb4b32c8a69930e4272e0cee4b81ca97c3354
maged <maged12345>
2021-12-06 11:13:48 -0800

    fixing slider through guard let statement

--- Filter Image Demo/ViewController.swift
+++ Filter Image Demo/ViewController.swift
@@ -48,8 +48,8 @@ class ViewController: UIViewController {
            if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intenditySlider.value * 200, forKey: kCIInputRadiusKey) }
            if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intenditySlider.value * 10, forKey: kCIInputScaleKey) }
            if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
-
-           if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+        guard let outputImage = currentFilter.outputImage else {return}
+           if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
                let processedImage = UIImage(cgImage: cgimg)
                self.imageView.image = processedImage
            }
@@ -69,7 +69,7 @@ class ViewController: UIViewController {
         applyProcessing()
     }
     @IBAction func sliderAction(_ sender: UISlider) {
-        
+       
             applyProcessing()
        }
    
